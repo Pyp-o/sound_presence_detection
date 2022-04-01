ec800f966c1e64a23029923121fded1ce2ec645c
raspberrypi3PresenceDetector <raspberrypi3.presencesensor@gmail.com>
2022-04-01 16:19:36 +0200

    change folders for logs update

--- logsGitUpdate.sh
+++ logsGitUpdate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-git -C /home/pyp/Documents/presence_detection/gitRepository/sound_presence_detection commit -m "logs update" -o ./logs.txt
-git -C /home/pyp/Documents/presence_detection/gitRepository/sound_presence_detection push origin main
+git -C /home/pi/Documents/presenceDetectorGit/sound_presence_detection commit -m "logs update" -o ./logs.txt
+git -C /home/pi/Documents/presenceDetectorGit/sound_presence_detection push origin main
