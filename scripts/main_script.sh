9a6e5f1faf0b84cb579b1552847cc2a4b334283c
raspberrypi3PresenceDetector <raspberrypi3.presencesensor@gmail.com>
2022-04-05 10:03:22 +0200

    resolution bugs

--- scripts/main_script.sh
+++ scripts/main_script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 bash /home/pi/Documents/presenceDetectorGit/sound_presence_detection/scripts/send.sh &
-python3 /home/pi/Documents/presenceDetectorGit/sound_presence_detection//scripts/receive_data_arduino.py &
+python3 /home/pi/Documents/presenceDetectorGit/sound_presence_detection/scripts/receive_data_arduino.py &
--- scripts/receive_data_arduino.py
+++ scripts/receive_data_arduino.py
@@ -7,5 +7,5 @@ ser.reset_input_buffer()
 while True:
 	if ser.in_waiting > 0 :
 		line = ser.readline().decode('utf-8').rstrip()
-		print(line)
+		#print(line)
 		subprocess.check_call(['/home/pi/Documents/presenceDetectorGit/sound_presence_detection/scripts/write_log.sh', line])
